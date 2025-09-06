30d643f44d005e6fb4dbf69246a3b5fec8555cad
Archana-reddy09 <archanachintala82@gmail.com>
2025-09-06 12:52:02 +0530

    rest.api created by class based

--- eprod/settings.py
+++ eprod/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'eapp',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,8 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
 ROOT_URLCONF = 'eprod.urls'
 
 TEMPLATES = [
